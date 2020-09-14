0e0019fef5ec83018602f6ea388dbe7d348af456
aet37 <aet37@pitt.edu>
2020-09-14 16:18:12 -0400

    Start to lay out acquire steps

---
+++ acquire.py
@@ -0,0 +1,19 @@
+'''
+	@author: Andrew Toader
+	@date: 14 September 2020
+	@purpose: simple module to acquire video on raspberry pi
+'''
+
+import os
+
+# display welcome message:
+print("Welcome to pupilometry-raspi acquire video funciton.")
+print("Enter information requested below.")
+
+# get the video time
+time = int(input("Time (ms): "))
+
+# get the 
+
+
+os.system("")
